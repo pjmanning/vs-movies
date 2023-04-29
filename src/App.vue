645646cfd3557adae7b0a0b6c5dfd1b285078e99
PJ Manning <phil.manning11@gmail.com>
2023-04-29 17:16:46 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
